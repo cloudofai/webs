c9a1928172edd8342ad90e1c4ac8be09946a9ddd
cloudofai <knutadobe@web.de>
2025-05-15 18:32:15 -0400

    Add files via upload

--- GTFD-webserver.sh
+++ GTFD-webserver.sh
@@ -1179,7 +1179,7 @@ chmod 644 "$oO_LOG_NFTABLES"
 chown root:adm "$oO_LOG_NFTABLES"
 
 configure_nginx_ssl() {
-    log "Configuring Nginx SSL with strict HTTPS enforcement and SOCKS5 proxy..." "$oO_LOG_FILE"
+    log "$LOG_LEVEL_INFO" "Configuring Nginx SSL with strict HTTPS enforcement and SOCKS5 proxy..." "$oO_LOG_FILE"
     local default_site="/etc/nginx/sites-enabled/default"
     binary_remote_addr="\$binary_remote_addr"
 
@@ -1201,46 +1201,46 @@ configure_nginx_ssl() {
 
     # Check if the default site exists
     if [ -L "$default_site" ] || [ -f "$default_site" ]; then
-        log  "Found default site at $default_site. Removing it..." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_INFO" "Found default site at $default_site. Removing it..." "$oO_LOG_FILE"
         rm "$default_site"
-        log "Default site removed successfully." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_WARNING" "Default site removed successfully." "$oO_LOG_FILE"
     else
-        log  "Default site not found at $default_site. No action needed." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_WARNING" "Default site not found at $default_site. No action needed." "$oO_LOG_FILE"
     fi
 
     # Ensure SSL certificates
     if [ ! -f /etc/ssl/private/nginx-selfsigned.key ] || [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ] || [ ! -f /etc/ssl/certs/dhparam.pem ]; then
-        log "Required SSL files missing. Generating SSL certificates..." "$oO_LOG_FILE"
+        log $LOG_LEVEL_INFO "Required SSL files missing. Generating SSL certificates..." "$oO_LOG_FILE"
         configure_openssl
         if ! my_command; then
             log "$LOG_LEVEL_WARNING" "Failed to generate SSL certificates. Skipping Nginx configuration." "$oO_LOG_FILE"
             return 1
         fi
     else
-        log "SSL certificates already exist. Skipping generation." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_WARNING""SSL certificates already exist. Skipping generation." "$oO_LOG_FILE"
     fi
 
     # Configure global Nginx settings
     local nginx_global_conf="/etc/nginx/nginx.conf"
     if [ ! -f "$nginx_global_conf.bak" ]; then
         cp "$nginx_global_conf" "$nginx_global_conf.bak"
-        log "Backup of nginx.conf created at $nginx_global_conf.bak" "$oO_LOG_FILE"
+        log "$LOG_LEVEL_INFO" "Backup of nginx.conf created at $nginx_global_conf.bak" "$oO_LOG_FILE"
     fi
 
     # Ensure the limit_req_zone directive exists in the global http block
     if ! grep -q "limit_req_zone" "$nginx_global_conf"; then
         sed -i "/^http {/a \    limit_req_zone $binary_remote_addr zone=one:10m rate=10r/s;" "$nginx_global_conf"
-        log "Added rate limiting directive to Nginx global http block." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_INFO" "Added rate limiting directive to Nginx global http block." "$oO_LOG_FILE"
     else
-        log "Rate limiting directive already exists in Nginx configuration, skipping." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_WARNING" "Rate limiting directive already exists in Nginx configuration, skipping." "$oO_LOG_FILE"
     fi
 
     # Ensure the log_format directive exists in the global http block
     if ! grep -q "log_format proxy_logs" "$nginx_global_conf"; then
         sed -i "/^http {/a \    log_format proxy_logs '\''[\$time_local] \$remote_addr: \$remote_port -> \$server_addr: \$server_port '\''\n                       '\"\$request" \$status \$body_bytes_sent'\\n                       '\"\$http_referer\" \"\$http_user_agent" SSL: \$ssl_cipher \$ssl_protocol';" "$nginx_global_conf"
-        log "Added log_format directive to Nginx global http block." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_INFO" "Added log_format directive to Nginx global http block." "$oO_LOG_FILE"
     else
-        log "Log_format directive already exists in Nginx configuration, skipping." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_WARNING" "Log_format directive already exists in Nginx configuration, skipping." "$oO_LOG_FILE"
     fi
 
     # Configure Nginx SSL snippet 
@@ -1351,9 +1351,9 @@ EOF
     local NGX_SITES_ENABLED="/etc/nginx/sites-enabled/tor_proxy"
     if [ ! -f "$NGX_SITES_ENABLED" ]; then
         ln -s "$NGX_CONF" "$NGX_SITES_ENABLED"
-        log "Linked $NGX_CONF to $NGX_SITES_ENABLED." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_INFO" "Linked $NGX_CONF to $NGX_SITES_ENABLED." "$oO_LOG_FILE"
     else
-        log "Nginx site configuration already enabled, skipping." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_WARNING" "Nginx site configuration already enabled, skipping." "$oO_LOG_FILE"
     fi
 
     # Test Nginx configuration
@@ -1364,20 +1364,20 @@ EOF
 
     # Start Nginx to apply changes
     if systemctl start nginx; then
-        log "Nginx started successfully with updated configuration." "$oO_LOG_FILE"
+        log "$LOG_LEVEL_INFO" "Nginx started successfully with updated configuration." "$oO_LOG_FILE"
     else
         log "$LOG_LEVEL_WARNING" "Failed to start Nginx. Check the service status." "$oO_LOG_FILE"
         return 1
     fi
 
-    log "Nginx SSL with strict HTTPS and SOCKS5 proxy configured successfully." "$oO_LOG_FILE"
+    log "$LOG_LEVEL_WARNING" "Nginx SSL with strict HTTPS and SOCKS5 proxy configured successfully." "$oO_LOG_FILE"
     return 0
 }
 
 # Create configure_privoxy for socks5 traffic
 privoxy_conf() {
     local PRIVOXY_CONF="/etc/privoxy/config"
-    log "Creating privoxy config..." "$oO_LOG_FILE"
+    log "$LOG_LEVEL_INFO" "Creating privoxy config..." "$oO_LOG_FILE"
     cat << 'EOF' > "$PRIVOXY_CONF"
 # Weiterleitung an Tor (SOCKS5-Proxy)
 forward-socks5t / 127.0.0.1:9050 .
@@ -1389,7 +1389,7 @@ EOF
     # Set permissions for the configuration file
     chmod 644 "$PRIVOXY_CONF"
     chown root:root "$PRIVOXY_CONF"
-    log "Creating privoxy config successful..." "$oO_LOG_FILE"
+    log "$LOG_LEVEL_INFO" "Creating privoxy config successful..." "$oO_LOG_FILE"
 }
 
 echo "============================================================================="
